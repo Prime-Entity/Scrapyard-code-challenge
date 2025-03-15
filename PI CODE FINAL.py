bda50251658c6bf31fc6fcde9b458e2d58813dd8
Prime-Entity <mrshadowjr101@gmail.com>
2025-03-15 18:35:19 +0000

    Add files via upload

---
+++ PI CODE FINAL.py
@@ -0,0 +1,183 @@
+import keyboard
+import time
+import sys
+import io
+import pyautogui
+
+sys=stdin = io.StringIO()
+
+messages = [""]
+running = True
+
+while running:
+    if keyboard.is_pressed('a'):
+        pyautogui.typewrite("wful_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed ('A'):
+        pyautogui.typewrite ("wful_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('b'):
+        pyautogui.typewrite ("rat_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('B'):
+        pyautogui.typewrite ("rat_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('c'):
+        pyautogui.typewrite ("reep_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('C'):
+        pyautogui.typewrite ("reep_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('d'):
+        pyautogui.typewrite ("isapiontment_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('D'):
+        pyautogui.typewrite ("isapiontment_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('e'):
+        pyautogui.typewrite ("l_bozo_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('E'):
+        pyautogui.typewrite ("l_bozo_")
+        time.sleep(0.9)  
+    elif keyboard.is_pressed('f'):
+        pyautogui.typewrite ("art_")
+        time.sleep(0.9)  
+    elif keyboard.is_pressed('F'):
+        pyautogui.typewrite ("art_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('g'):
+        pyautogui.typewrite ("rundge_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('G'):
+        pyautogui.typewrite ("rundge_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('h'):
+        pyautogui.typewrite ("orrible_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('H'):
+        pyautogui.typewrite ("orrible_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('i'):
+        pyautogui.typewrite ("diot_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('I'):
+        pyautogui.typewrite ("diot_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('j'):
+        pyautogui.typewrite ("erk_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('J'):
+        pyautogui.typewrite ("erk_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('k'):
+        pyautogui.typewrite ("nuckle_head_")
+        time.sleep(0.9) 
+    elif keyboard.is_pressed('K'):
+        pyautogui.typewrite ("nuckle_head_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('l'):
+        pyautogui.typewrite ("oon_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('L'):
+        pyautogui.typewrite ("oon_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('m'):
+        pyautogui.typewrite ("oron_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('M'):
+        pyautogui.typewrite ("oron_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('n'):
+        pyautogui.typewrite ("erd_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('N'):
+        pyautogui.typewrite ("erd_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('o'):
+        pyautogui.typewrite ("blivous_twit_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('O'):
+        pyautogui.typewrite ("blivous_twit_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('p'):
+        pyautogui.typewrite ("athetic_excuse_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('p'):
+        pyautogui.typewrite ("athetic_excuse_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('q'):
+        pyautogui.typewrite ("uestional_fool_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('Q'):
+        pyautogui.typewrite ("uestional_fool_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('r'):
+        pyautogui.typewrite ("idiculous_imbicile_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('R'):
+        pyautogui.typewrite ("idiculous_imbicile_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('s'):
+        pyautogui.typewrite ("tupid_monkey_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('S'):
+        pyautogui.typewrite ("tupid_monkey_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('t'):
+        pyautogui.typewrite ("awdry_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('T'):
+        pyautogui.typewrite ("awdry_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('u'):
+        pyautogui.typewrite ("seless_dolt_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('U'):
+        pyautogui.typewrite ("seless_dolt_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('v'):
+        pyautogui.typewrite ("ain_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('V'):
+        pyautogui.typewrite ("ain_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('w'):
+        pyautogui.typewrite ("orthless_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('W'):
+        pyautogui.typewrite ("orthless_fool")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('x'):
+        pyautogui.typewrite ("enophobic_fool_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('X'):
+        pyautogui.typewrite ("enophobic_fool_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('y'):
+        pyautogui.typewrite ("ou_absolute_donut_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('Y'):
+        pyautogui.typewrite ("ou_ablosute_donut_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('z'):
+        pyautogui.typewrite ("ero_IQ_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('Z'):
+        pyautogui.typewrite ("ero_IQ_")
+        time.sleep(0.9)
+    elif keyboard.is_pressed('space'):
+        pyautogui.typewrite ("_")
+    elif keyboard.is_pressed('backspace'):
+        pyautogui.typewrite ("NO_")
+    elif keyboard.is_pressed('delete'):
+        pyautogui.typewrite ("NO_")
+    if keyboard.is_pressed ('esc'):
+        running = False
+
+
+with open ("Insult.txt","w") as file:
+   
+    for line in messages:
+        print (line)
+        file.write(line)
